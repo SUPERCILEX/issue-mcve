2144f7bc45409cba881717e56a213c1c8da9ed86
Alex Saveau <saveau.alexandre@gmail.com>
2018-02-20 16:50:10 -0800

    Request: 1-0424000021155
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- app/src/main/java/com/supercilex/test/MainActivity.kt
+++ app/src/main/java/com/supercilex/test/MainActivity.kt
@@ -2,10 +2,34 @@ package com.supercilex.test
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.TextView
+import com.google.firebase.firestore.FirebaseFirestore
+import org.jetbrains.anko.find
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseFirestore.setLoggingEnabled(true)
+        val root = FirebaseFirestore.getInstance().collection("test")
+
+        root.firestore.batch().apply {
+            set(root.document(), mapOf("owners" to mapOf("a" to Date())))
+            set(root.document(), mapOf("owners" to mapOf("b" to Date())))
+            set(root.document(), mapOf("owners" to mapOf("c" to Date())))
+            commit()
+        }
+
+        root.orderBy("uid").addSnapshotListener { snapshot, e ->
+            if (e != null) {
+                Log.e("TAG", "Error fetching documents", e)
+                return@addSnapshotListener
+            }
+
+            find<TextView>(R.id.text).text = snapshot.map { it.data }.joinToString()
+        }
     }
 }
