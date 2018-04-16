f4c182670a4800c9eeeda6e8b4cdd6c9a7a7870f
Alex Saveau <saveau.alexandre@gmail.com>
2018-04-16 15:13:34 -0700

    [4-8469000021775] socket not closed on I/O exception
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- app/src/main/java/com/supercilex/test/MainActivity.kt
+++ app/src/main/java/com/supercilex/test/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.supercilex.test
 
+import android.os.Build
 import android.os.Bundle
+import android.os.StrictMode
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.widget.TextView
+import android.widget.Button
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
 import org.jetbrains.anko.find
 import java.util.Date
@@ -12,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findbugs()
 
         FirebaseFirestore.setLoggingEnabled(true)
         val root = FirebaseFirestore.getInstance().collection("test")
@@ -23,13 +28,49 @@ class MainActivity : AppCompatActivity() {
             commit()
         }
 
-        root.orderBy("uid").addSnapshotListener { snapshot, e ->
+        //////////////// INSTRUCTIONS ////////////////
+        // 1. Open app
+        // 2. Go offline and look for StrictMode logs
+        // 3. If you don't see anything, press the button somewhere along
+
+        query(root)
+        find<Button>(R.id.button).setOnClickListener { query(root) }
+    }
+
+    private fun query(root: CollectionReference) {
+        root.orderBy("uid").addSnapshotListener { _, e ->
             if (e != null) {
                 Log.e("TAG", "Error fetching documents", e)
                 return@addSnapshotListener
             }
+        }
+    }
 
-            find<TextView>(R.id.text).text = snapshot.map { it.data }.joinToString()
+    private fun findbugs() {
+        if (BuildConfig.DEBUG) {
+            val vmBuilder = StrictMode.VmPolicy.Builder()
+                    .detectActivityLeaks()
+                    .detectLeakedClosableObjects()
+                    .detectLeakedRegistrationObjects()
+                    .detectLeakedSqlLiteObjects()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                vmBuilder.detectCleartextNetwork()
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vmBuilder.detectContentUriWithoutPermission()
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                vmBuilder.detectFileUriExposure()
+            }
+            StrictMode.setVmPolicy(vmBuilder.penaltyLog().build())
+
+            StrictMode.setThreadPolicy(
+                    StrictMode.ThreadPolicy.Builder()
+                            .detectAll()
+                            .penaltyDeath()
+                            .penaltyLog()
+                            .build()
+            )
         }
     }
 }
