5bd1cfd39a15631436d8235aa72c385f29c8149d
Alex Saveau <saveau.alexandre@gmail.com>
2019-03-06 14:14:13 -0800

    Update stuff
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,6 +1,6 @@
 @Suppress("MayBeConstant")
 object Config {
-    private const val kotlinVersion = "1.3.11"
+    private const val kotlinVersion = "1.3.21"
 
     object SdkVersions {
         val compile = 28
@@ -9,10 +9,10 @@ object Config {
     }
 
     object Plugins {
-        val android = "com.android.tools.build:gradle:3.4.0-alpha10"
+        val android = "com.android.tools.build:gradle:3.5.0-alpha06"
         val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
         val google = "com.google.gms:google-services:4.2.0"
-        val publishing = "com.github.triplet.gradle:play-publisher:2.0.0"
+        val publishing = "com.github.triplet.gradle:play-publisher:2.1.1"
     }
 
     object Libs {
@@ -28,9 +28,9 @@ object Config {
         }
 
         object Support {
-            val multidex = "com.android.support:multidex:1.0.3"
-            val design = "com.android.support:design:28.0.0"
-            val constraint = "com.android.support.constraint:constraint-layout:1.1.3"
+            val multidex = "androidx.multidex:multidex:2.0.1"
+            val design = "androidx.appcompat:appcompat:1.0.2"
+            val constraint = "androidx.constraintlayout:constraintlayout:1.1.3"
         }
     }
 }
