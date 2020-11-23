7b41c9269ded08dadcf88c73ab3277180479d749
Alex Saveau <saveau.alexandre@gmail.com>
2020-11-22 20:15:33 -0800

    Update deps
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     `lifecycle-base`
-    id("com.github.ben-manes.versions") version "0.28.0"
+    id("com.github.ben-manes.versions") version "0.36.0"
 
     kotlin("jvm") version "1.4.20" apply false
     id("com.android.application") version "4.1.0" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.3.1" apply false
-    id("com.google.gms.google-services") version("4.3.3") apply false
+    id("com.google.gms.google-services") version("4.3.4") apply false
     id("com.github.triplet.play") version "3.0.0" apply false
     id("com.supercilex.gradle.versions") version "0.7.0" apply false
 }
--- dependencies/build.gradle.kts
+++ dependencies/build.gradle.kts
@@ -3,16 +3,16 @@ plugins {
 }
 
 dependencies.constraints {
-    val coroutinesVersion = "1.3.7"
+    val coroutinesVersion = "1.4.1"
     api("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:$coroutinesVersion")
 
-    val navVersion = "2.3.0-rc01"
+    val navVersion = "2.3.1"
     api("androidx.multidex:multidex:2.0.1")
-    api("androidx.appcompat:appcompat:1.2.0-rc01")
-    api("androidx.constraintlayout:constraintlayout:2.0.0-beta7")
+    api("androidx.appcompat:appcompat:1.3.0-alpha02")
+    api("androidx.constraintlayout:constraintlayout:2.1.0-alpha1")
     api("androidx.navigation:navigation-fragment-ktx:$navVersion")
     api("androidx.navigation:navigation-ui-ktx:$navVersion")
 
-    api("com.google.firebase:firebase-analytics:17.4.3")
+    api("com.google.firebase:firebase-analytics:18.0.0")
 }
